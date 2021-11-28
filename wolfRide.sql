95d0321b316c16e4b78a903884d35b53c9a36354
Armando Yanez <ayanez20@wou.edu>
2021-11-28 15:07:15 -0800

    Added remaining tables from ERD for wolfRide

--- wolfRide.sql
+++ wolfRide.sql
@@ -12,7 +12,81 @@ CREATE TABLE Credentials (
 	[Password] VARCHAR(512) NOT NULL
 );
 
+CREATE TABLE UserType (
+	UserTypeID INT PRIMARY KEY,
+	UserType VarChar(8)
+);
+
+CREATE TABLE RideStatus (
+	RideStatusID INT PRIMARY KEY,
+	RideStatus VARCHAR(256)
+);
+
+CREATE TABLE PaymentStatus (
+	PaymentStatusID INT PRIMARY KEY,
+	PaymentStatus VARCHAR(256)
+);
+
+CREATE TABLE MakeModel (
+	MakeModelID INT PRIMARY KEY,
+	Make VARCHAR(256),
+	Model VARCHAR(256)
+);
+
+CREATE TABLE Vehicle (
+	VehicleID INT PRIMARY KEY,
+	LicensePlate VARCHAR(256),
+
+	MakeModelID INT FOREIGN KEY REFERENCES MakeModel(MakeModelID)
+);
+
+CREATE TABLE Locale (
+	LocaleID INT PRIMARY KEY,
+	City VARCHAR(256),
+	[STATE] VARCHAR(2)
+);
+
+CREATE TABLE Zip (
+	ZipID INT PRIMARY KEY,
+	ZipCode INT
+);
+
+CREATE TABLE [Address] (
+	AddressID INT PRIMARY KEY,
+	Line1 VARCHAR(256),
+	Line2 VARCHAR(256),
+	LocaleID INT FOREIGN KEY REFERENCES Locale(LocaleID),
+	ZipID INT FOREIGN KEY REFERENCES Zip(ZipID)
+);
+
+CREATE TABLE [User] (
+	UserID INT PRIMARY KEY,
+	FullName VARCHAR(256),
+	PhoneNumber VarChar(12),
+	Email VarChar(256),
+	Balance MONEY,
+
+	UserTypeID INT FOREIGN KEY REFERENCES UserType(UserTypeID),
+	AddressID INT FOREIGN KEY REFERENCES [Address](AddressID),
+	CredentialsID VARCHAR(256) FOREIGN KEY REFERENCES Credentials(UserName)
+);
+
+CREATE TABLE Ride (
+	RideID INT PRIMARY KEY,
+	NumOfPassengers INT,
+	EstimatedTimeOfArrival TIME,
+	PickupTime TIME,
+	Destination VARCHAR(256),
+
+	Rider INT FOREIGN KEY REFERENCES [USER](UserID),
+	Driver INT FOREIGN KEY REFERENCES [USER](UserID),
+	RideStatus INT FOREIGN KEY REFERENCES RideStatus(RideStatusID),
+	PaymentStatus INT FOREIGN KEY REFERENCES PaymentStatus(PaymentStatusID)
+);
+
 INSERT INTO Credentials
 VALUES ('Armando Yanez', 'Password!'), ('Peter Parker', 'Spider-Man');
 
 SELECT * FROM Credentials;
+
+--DROP TABLE [USER];
